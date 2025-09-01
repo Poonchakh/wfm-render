0173d4a94c865cb026fcef53aad3e5e5b880ddee
Poonchakh <fed.stoyan@gmail.com>
2025-09-01 17:39:53 +0200

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,1 +1,10 @@
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/prices", (req, res) => {
+  res.json({ message: "Render endpoint работает!" });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
