604efb45e4da5424a446c71143c097b523b9d349
Poonchakh <fed.stoyan@gmail.com>
2025-09-02 00:05:27 +0200

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,3 +1,9 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import express from "express";
 import fetch from "node-fetch";
 
@@ -221,3 +227,6 @@ app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
   updateCache(); // загружаем данные сразу при старте
 });
+
+// Чтобы отдавать файлы из /public
+app.use(express.static(path.join(__dirname, "../public")));
